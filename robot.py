98b272d76868d13517f57cde11b2bbe6c0f7d68c
Gabe3L <gabe3lynch@gmail.com>
2025-05-13 09:59:10 -0400

    Updated imports and licensing

--- robot.py
+++ robot.py
@@ -1,23 +1,19 @@
-#!/usr/bin/env python3
-#
-# Copyright (c) FIRST and other WPILib contributors.
+# Copyright (c) FRC Team 3756 RamFerno.
 # Open Source Software; you can modify and/or share it under the terms of
-# the WPILib BSD license file in the root directory of this project.
-#
+# the license viewable in the root directory of this project.
 
-import wpilib
-import wpilib.drive
-import phoenix5
+from wpilib import XboxController, Timer, TimedRobot
+from phoenix5 import TalonSRX, TalonSRXControlMode
 
-class MyRobot(wpilib.TimedRobot):
+class MyRobot(TimedRobot):
     def robotInit(self):
-        self.leftDriveLeader = phoenix5.TalonSRX(6)
-        self.leftDriveFront = phoenix5.TalonSRX(5)
-        self.leftDriveBack = phoenix5.TalonSRX(4)
+        self.leftDriveLeader = TalonSRX(6)
+        self.leftDriveFront = TalonSRX(5)
+        self.leftDriveBack = TalonSRX(4)
 
-        self.rightDriveLeader = phoenix5.TalonSRX(1)
-        self.rightDriveFront = phoenix5.TalonSRX(2)
-        self.rightDriveBack = phoenix5.TalonSRX(3)
+        self.rightDriveLeader = TalonSRX(1)
+        self.rightDriveFront = TalonSRX(2)
+        self.rightDriveBack = TalonSRX(3)
 
         self.leftDriveFront.follow(self.leftDriveLeader)
         self.leftDriveBack.follow(self.leftDriveLeader)
@@ -25,8 +21,8 @@ class MyRobot(wpilib.TimedRobot):
         self.rightDriveFront.follow(self.rightDriveLeader)
         self.rightDriveBack.follow(self.rightDriveLeader)
 
-        self.controller = wpilib.XboxController(0)
-        self.timer = wpilib.Timer()
+        self.controller = XboxController(0)
+        self.timer = Timer()
 
         self.rightDriveLeader.setInverted(True)
 
@@ -35,18 +31,15 @@ class MyRobot(wpilib.TimedRobot):
 
     def autonomousPeriodic(self):
         if self.timer.get() < 2.0:
-            self.leftDriveLeader.set(phoenix5.TalonSRXControlMode.MotionMagic, 0.5)
-            self.rightDriveLeader.set(phoenix5.TalonSRXControlMode.MotionMagic, 0.5)
+            self.leftDriveLeader.set(TalonSRXControlMode.MotionMagic, 0.5)
+            self.rightDriveLeader.set(TalonSRXControlMode.MotionMagic, 0.5)
         else:
-            self.leftDriveLeader.set(phoenix5.TalonSRXControlMode.MotionMagic, 0.0)
-            self.rightDriveLeader.set(phoenix5.TalonSRXControlMode.MotionMagic, 0.0)
+            self.leftDriveLeader.set(TalonSRXControlMode.MotionMagic, 0.0)
+            self.rightDriveLeader.set(TalonSRXControlMode.MotionMagic, 0.0)
 
     def teleopInit(self):
         pass
 
     def teleopPeriodic(self):
-        self.leftDriveLeader.set(phoenix5.TalonSRXControlMode.MotionMagic, 0.5)
-        self.rightDriveLeader.set(phoenix5.TalonSRXControlMode.MotionMagic, 0.5)
-
-if __name__ == "__main__":
-    wpilib.run(MyRobot)
+        self.leftDriveLeader.set(TalonSRXControlMode.MotionMagic, 0.5)
+        self.rightDriveLeader.set(TalonSRXControlMode.MotionMagic, 0.5)
