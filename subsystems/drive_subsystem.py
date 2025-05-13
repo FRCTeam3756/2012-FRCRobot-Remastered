3a99895cd9be4f2b1ba9b6dbf5db27b83a34386b
Gabe3L <gabe3lynch@gmail.com>
2025-05-13 13:41:08 -0400

    Added turning to the drive command

--- subsystems/drive_subsystem.py
+++ subsystems/drive_subsystem.py
@@ -1,3 +1,5 @@
+import math
+
 from commands2 import Subsystem
 from phoenix5 import TalonSRX, TalonSRXControlMode
 
@@ -25,11 +27,13 @@ class DriveSubsystem(Subsystem):
         self.rightDriveFront.follow(self.rightDriveLeader)
         self.rightDriveBack.follow(self.rightDriveLeader)
 
-    def drive(self, xInput, yInput) -> None:
-        """Drive the robot using joystick inputs."""
-
-        left_speed = xInput * (-yInput) * DriveConstants.MAX_SPEED
-        right_speed = xInput * yInput * DriveConstants.MAX_SPEED
+    def drive(self, xInput: float, yInput: float) -> None:
+        left_speed = yInput * self.compute_turn(xInput, isLeft=True) * DriveConstants.MAX_SPEED
+        right_speed = yInput * self.compute_turn(xInput, isLeft=False) * DriveConstants.MAX_SPEED
 
         self.leftDriveLeader.set(TalonSRXControlMode.PercentOutput, left_speed)
         self.rightDriveLeader.set(TalonSRXControlMode.PercentOutput, right_speed)
+    
+    @staticmethod
+    def compute_turn(xInput: float, isLeft: bool) -> float:
+        return ((math.cos((((0.5 if isLeft else -0.5) * math.pi) * xInput) - (0.5 * math.pi)) + 1) / 2) * DriveConstants.MAX_SPEED
